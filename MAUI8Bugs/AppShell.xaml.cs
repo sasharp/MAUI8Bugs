9cd1d77c28fa82d9a72296bb258c9ac355514583
Alexander Filuk <af@am-its.com>
2024-10-16 17:04:56 +0200

    - routes added;

--- MAUI8Bugs/AppShell.xaml.cs
+++ MAUI8Bugs/AppShell.xaml.cs
@@ -7,5 +7,30 @@
             Routing.RegisterRoute("subpage", typeof(SubPage));
             InitializeComponent();
         }
+
+        private void Shell_Navigated(object sender, ShellNavigatedEventArgs e)
+        {
+
+        }
+
+        private void Shell_Navigating(object sender, ShellNavigatingEventArgs e)
+        {
+
+        }
+
+        private void Shell_NavigatedFrom(object sender, NavigatedFromEventArgs e)
+        {
+
+        }
+
+        private void Shell_NavigatingFrom(object sender, NavigatingFromEventArgs e)
+        {
+
+        }
+
+        private void Shell_NavigatedTo(object sender, NavigatedToEventArgs e)
+        {
+
+        }
     }
 }
